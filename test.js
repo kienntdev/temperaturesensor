3110901541268ebeec47aaa6e2b9d25532586cc4
kien.nguyen <kien.nguyen@ext.taylormadegolf.com>
2023-03-20 18:03:44 +0700

    update

--- test.js
+++ test.js
@@ -1,35 +1,41 @@
-const fs=require('fs');
- 
-const HWSENSORPREFIX='/sys/bus/w1/devices/';
- 
+const fs = require("fs");
+
+const HWSENSORPREFIX = "/sys/bus/w1/devices/";
+const HWMON = "hwmon";
+
 async function getSensors() {
-  let sensors=[];
+  let sensors = [];
   const dir = await fs.promises.opendir(`${HWSENSORPREFIX}`);
   for await (const dirent of dir) {
-    if (dirent.name.slice(0,3)=='28-') {
-        sensors.push(dirent.name);
+    if (dirent.name.slice(0, 3) == "28-") {
+      sensors.push(dirent.name);
     }
   }
   return sensors;
 }
- 
+
 // Print temperature reading from a given 1-Wire sensor
- 
+
 async function printTemp(sensor) {
-  const dir = await fs.promises.opendir(`${HWSENSORPREFIX}/${sensor}/hwmon`);
+  const dir = await fs.promises.opendir(`${HWSENSORPREFIX}/${sensor}/${HWMON}`);
   for await (const dirent of dir) {
-    if (dirent.name.slice(0, 5)=='hwmon') {
-      let temp = parseFloat(await fs.promises.readFile(`${HWSENSORPREFIX}/${sensor}/hwmon/${dirent.name}/temp1_input`))/1000.0;
+    if (dirent.name.slice(0, 5) == HWMON) {
+      let temp =
+        parseFloat(
+          await fs.promises.readFile(
+            `${HWSENSORPREFIX}/${sensor}/${HWMON}/${dirent.name}/temp1_input`
+          )
+        ) / 1000.0;
       console.log(`${dirent.name} (${sensor}): ${temp}`);
     }
   }
 }
- 
+
 (async function () {
   try {
-    let a=await getSensors();
-    a.forEach(x => printTemp(x));
-  } catch(e) {
+    let a = await getSensors();
+    a.forEach((x) => printTemp(x));
+  } catch (e) {
     console.error(`Error: ${e}`);
   }
 })();
