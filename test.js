f78e38a4b33ee262383edc23b564e97db3bbd9a9
kien.nguyen <kien.nguyen@ext.taylormadegolf.com>
2023-03-20 18:48:44 +0700

    use ds18b20

--- test.js
+++ test.js
@@ -1,40 +1,41 @@
-const fs = require("fs");
+var ds18b20 = require('ds18b20');
 
-const HWSENSORPREFIX = "/sys/bus/w1/devices/";
-const HWMON = "hwmon";
+// const fs = require("fs");
 
-async function getSensors() {
-  let sensors = [];
-  const dir = await fs.promises.opendir(`${HWSENSORPREFIX}`);
-  for await (const dirent of dir) {
-    if (dirent.name.slice(0, 3) == "28-") {
-      sensors.push(dirent.name);
-    }
-  }
-  return sensors;
-}
+// const HWSENSORPREFIX = "/sys/bus/w1/devices/";
+// const HWMON = "hwmon";
 
-// Print temperature reading from a given 1-Wire sensor
+// async function getSensors() {
+//   let sensors = [];
+//   const dir = await fs.promises.opendir(`${HWSENSORPREFIX}`);
+//   for await (const dirent of dir) {
+//     if (dirent.name.slice(0, 3) == "28-") {
+//       sensors.push(dirent.name);
+//     }
+//   }
+//   return sensors;
+// }
 
-async function printTemp(sensor) {
-  const dir = await fs.promises.opendir(`${HWSENSORPREFIX}/${sensor}/${HWMON}`);
-  for await (const dirent of dir) {
-    if (dirent.name.slice(0, 5) == HWMON) {
-      let temp =
-        parseFloat(
-          await fs.promises.readFile(
-            `${HWSENSORPREFIX}/${sensor}/${HWMON}/${dirent.name}/temp1_input`
-          )
-        ) / 1000.0;
-      console.log(`${dirent.name} (${sensor}): ${temp}`);
-    }
-  }
-}
+// // Print temperature reading from a given 1-Wire sensor
+// async function printTemp(sensor) {
+//   const dir = await fs.promises.opendir(`${HWSENSORPREFIX}/${sensor}/${HWMON}`);
+//   for await (const dirent of dir) {
+//     if (dirent.name.slice(0, 5) == HWMON) {
+//       let temp =
+//         parseFloat(
+//           await fs.promises.readFile(
+//             `${HWSENSORPREFIX}/${sensor}/${HWMON}/${dirent.name}/temp1_input`
+//           )
+//         ) / 1000.0;
+//       console.log(`${dirent.name} (${sensor}): ${temp}`);
+//     }
+//   }
+// }
 
 (async function () {
   try {
-    let a = await getSensors();
-    a.forEach((x) => printTemp(x));
+    let ids = ds18b20.sensors();
+    ids.forEach((x) => ds18b20.temperatureSync(x));
   } catch (e) {
     console.error(`Error: ${e}`);
   }
