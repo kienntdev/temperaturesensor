d25470903eb7dda875f16cdbeaef732413f979ed
kien.nguyen <kien.nguyen@ext.taylormadegolf.com>
2023-03-21 18:30:14 +0700

    use ds18b20

--- test.js
+++ test.js
@@ -1,4 +1,19 @@
-var ds18b20 = require("ds18b20");
+const ds18b20 = require("ds18b20");
+const moment = require("moment");
+const fs = require("fs");
+
+const HWSENSORPREFIX = "/sys/bus/w1/devices/";
+
+async function getSensors() {
+  let sensors = [];
+  const dir = await fs.promises.opendir(`${HWSENSORPREFIX}`);
+  for await (const dirent of dir) {
+    if (dirent.name.slice(0, 3) == "28-") {
+      sensors.push(dirent.name);
+    }
+  }
+  return sensors;
+}
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -6,21 +21,18 @@ function sleep(ms) {
   });
 }
 
+function getDateTime() {
+  return moment().format("yyyy-MM-DD:HH:mm:ss");
+}
+
 (async function () {
   do {
     try {
-      ds18b20.sensors((err, sensors) => {
-        if (err == null && sensors !== undefined && sensors.length > 0) {
-          console.log(sensors);
-          sensors.forEach((i) => {
-            console.log(i);
-            const temp = ds18b20.temperatureSync(i, {});
-            console.log(temp);
-          });
-        } else {
-          console.error(`Error: ${err}`);
-        }
-      });
+      const sensors = await getSensors();
+      if (sensors.length > 0) {
+        const temp = ds18b20.temperatureSync(i, {});
+        console.log(`Time: ${getDateTime()} - ${temp}`);
+      }
     } catch (e) {
       console.error(`Error: ${e}`);
     }
